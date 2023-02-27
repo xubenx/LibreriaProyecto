83f2ccbc5067914be89271f8cc489ac6210f8159
xubenx <ruben7huerta@gmail.com>
2023-02-26 23:31:31 -0600

    Agregar archivos de proyecto.

---
+++ Shared/Models/Carrito.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaProyecto.Shared.Models
+{
+    public class Carrito
+    {
+        public List<Libro> Libros { get; set; } = new List<Libro>();
+        public decimal Total => (decimal)Libros.Sum(l => l.Precio);
+    }
+
+
+}
---
+++ Shared/Models/Libro.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibreriaProyecto.Shared.Models
+{
+    public class Libro
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Imagen { get; set; }
+        public float Precio { get; set; }
+        
+        public int Edicion { get; set; }
+        public string ISBN{ get; set; }
+        public string Editorial { get; set; }
+
+
+    }
+}
